79921bea2bfa9d74f6692ee363457d052f803fd3
Robert Savoie <59752101+RobertSavoie@users.noreply.github.com>
2022-12-09 16:34:57 -0500

    Add files via upload

---
+++ JavaLab4.java
@@ -0,0 +1,14 @@
+// Megan Gagliardi & Rob Savoie
+// 12/09/2022
+// OOP 3200
+// Java Lab 4: Inheritance
+
+public class JavaLab4 {
+	public static void main(String[] args) {		
+		Product p1 = new Product();
+		Perishable ps1 = new Perishable();
+		
+		p1.display();
+		ps1.display();
+	}
+}
